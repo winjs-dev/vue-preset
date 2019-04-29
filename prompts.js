35c3d381be95ec21e60f6f586955ac1f02dabb36
cklwblove <lwbhtml@163.com>
2019-04-29 09:49:39 +0800

    更新版本

---
+++ prompts.js
@@ -0,0 +1,16 @@
+/**
+ *
+ * @authors liwb (you@example.org)
+ * @date    2019-04-29 09:31
+ * @version $ IIFE
+ */
+
+/* name module */
+
+(function ($, w, d) {
+
+  'use strict';
+
+  /* do something */
+
+}(jQuery, window, document));
