54f1a21793417e7b6b4228155ff48e92b52ede07
cklwblove <lwbhtml@163.com>
2019-06-13 10:10:16 +0800

    refactor: 优化 axios 请求的封装

--- generator/template/src/services/request.js
+++ generator/template/src/services/request.js
@@ -12,24 +12,43 @@ import axios from 'axios';
 import autoMatchBaseUrl from './autoMatchBaseUrl';
 import {TIMEOUT, HOME_PREFIX} from '../constant';
 
+const codeMessage = {
+  200: '服务器成功返回请求的数据。',
+  201: '新建或修改数据成功。',
+  202: '一个请求已经进入后台排队（异步任务）。',
+  204: '删除数据成功。',
+  400: '发出的请求有错误，服务器没有进行新建或修改数据的操作。',
+  401: '用户没有权限（令牌、用户名、密码错误）。',
+  403: '用户得到授权，但是访问是被禁止的。',
+  404: '发出的请求针对的是不存在的记录，服务器没有进行操作。',
+  406: '请求的格式不可得。',
+  410: '请求的资源被永久删除，且不会再得到的。',
+  422: '当创建一个对象时，发生一个验证错误。',
+  500: '服务器发生错误，请检查服务器。',
+  502: '网关错误。',
+  503: '服务不可用，服务器暂时过载或维护。',
+  504: '网关超时。',
+};
+
 // 添加一个请求拦截器 （于transformRequest之前处理）
-axios.interceptors.request.use(function (config) {
+axios.interceptors.request.use((config) => {
   // 以下代码，鉴权token,可根据具体业务增删。
   // demo示例:
   if (~config['url'].indexOf('operatorQry')) {
     config.headers['accessToken'] = 'de4738c67e1bb450be71b660f0716aa4675860cec1ff9bc23d800efb40519cf3';
   }
   return config;
-}, function (error) {
+}, (error) => {
   // 当出现请求错误是做一些处理
   return Promise.reject(error);
 });
 
 // 添加一个返回拦截器 （于transformResponse之后处理）
 // 返回的数据类型默认是json，若是其他类型（text）就会出现问题，因此用try,catch捕获异常
-axios.interceptors.response.use(function (response) {
+axios.interceptors.response.use((response) => {
+  // 业务处理（成功或者失败）
   return checkStatus(response);
-}, function (error) {
+}, (error) => {
   // 对返回的错误进行一些处理
   return Promise.reject(checkStatus(error));
 });
@@ -37,60 +56,20 @@ axios.interceptors.response.use(function (response) {
 function checkStatus(response) {
   // 如果http状态码正常，则直接返回数据
   if (response) {
-    // -1000 自己定义，连接错误的status
-    const status = response.status || -1000;
+    const { status, statusText } = response;
     if ((status >= 200 && status < 300) || status === 304) {
       // 如果不需要除了data之外的数据，可以直接 return response.data
       return response.data;
-    } else {
-      let errorInfo = '';
-      switch (status) {
-        case -1:
-          errorInfo = '远程服务响应失败,请稍后重试';
-          break;
-        case 400:
-          errorInfo = '400: 错误请求';
-          break;
-        case 401:
-          errorInfo = '401: 访问令牌无效或已过期';
-          break;
-        case 403:
-          errorInfo = '403: 拒绝访问';
-          break;
-        case 404:
-          errorInfo = '404：资源不存在';
-          break;
-        case 405:
-          errorInfo = '405: 请求方法未允许'
-          break;
-        case 408:
-          errorInfo = '408: 请求超时'
-          break;
-        case 500:
-          errorInfo = '500：访问服务失败';
-          break;
-        case 501:
-          errorInfo = '501：未实现';
-          break;
-        case 502:
-          errorInfo = '502：无效网关';
-          break;
-        case 503:
-          errorInfo = '503: 服务不可用'
-          break;
-        default:
-          errorInfo = `连接错误${status}`
-      }
-      return {
-        status,
-        msg: errorInfo
-      }
     }
+    return {
+      status,
+      msg: codeMessage[status] || statusText,
+    };
   }
   // 异常状态下，把错误信息返回去
   return {
     status: -404,
-    msg: '网络异常'
+    msg: '网络异常',
   };
 }
 
@@ -115,10 +94,7 @@ export default function request(url, {
 }) {
   const baseURL = autoMatchBaseUrl(prefix);
 
-  headers = Object.assign(method === 'get' ? {
-    'Accept': 'application/json',
-    'Content-Type': 'application/json; charset=UTF-8'
-  } : {
+  headers = Object.assign({
     'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
   }, headers);
 
@@ -143,6 +119,23 @@ export default function request(url, {
     }
   } else {
     delete defaultConfig.params;
+
+    const contentType = headers['Content-Type'];
+
+    if (typeof contentType !== 'undefined') {
+      if (~contentType.indexOf('multipart')) {
+        // 类型 `multipart/form-data;`
+        defaultConfig.data = data;
+      } else if (~contentType.indexOf('json')) {
+        // 类型 `application/json`
+        // 服务器收到的raw body(原始数据) "{name:"jhon",sex:"man"}"（普通字符串）
+        defaultConfig.data = JSON.stringify(data);
+      } else {
+        // 类型 `application/x-www-form-urlencoded`
+        // 服务器收到的raw body(原始数据) name=homeway&key=nokey
+        defaultConfig.data = Qs.stringify(data);
+      }
+    }
   }
 
   return axios(defaultConfig);
