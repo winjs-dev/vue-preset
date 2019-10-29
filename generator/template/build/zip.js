444b110f1c5359705d16eda6adabe4f340d30e28
cklwblove <lwbhtml@163.com>
2019-10-29 13:22:50 +0800

    打包构建zip包的时候，先删除 dist-zip目录，然后再创建

--- generator/template/build/zip.js
+++ generator/template/build/zip.js
@@ -17,7 +17,24 @@ const extractExtensionData = () => {
   };
 };
 
+const removeDir = function removeDir(dir) {
+  let files = fs.readdirSync(dir);
+  for (let i = 0; i < files.length; i++) {
+    let newPath = path.join(dir, files[i]);
+    let stat = fs.statSync(newPath);
+    if (stat.isDirectory()) {
+      // 如果是文件夹就递归下去
+      removeDir(newPath);
+    } else {
+      // 删除文件
+      fs.unlinkSync(newPath);
+    }
+  }
+  fs.rmdirSync(dir);//如果文件夹是空的，就将自己删除掉
+};
+
 const makeDestZipDirIfNotExists = () => {
+  removeDir(DEST_ZIP_DIR);
   if (!fs.existsSync(DEST_ZIP_DIR)) {
     fs.mkdirSync(DEST_ZIP_DIR);
   }
