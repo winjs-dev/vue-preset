7d0424b6a02c7faa4b31c8df24a8401cc06ff37d
cklwblove <lwbhtml@163.com>
2019-11-07 15:58:53 +0800

    路由拦截器增加 离线包 判断

--- generator/template/src/router/router.interceptor.js
+++ generator/template/src/router/router.interceptor.js
@@ -1,10 +1,16 @@
 import router from './.invoke/router';
+<%_ if (options.application === 'offline') { _%>
 import { isLightOS, navigateTo } from 'native-bridge-methods';
+<%_ } _%>
 
 router.beforeEach((to, from, next) => {
+<%_ if (options.application === 'offline') { _%>
   if (from.name && isLightOS()) {
     navigateTo({url: `${window.location.href.split('#')[0]}#${to.fullPath}`})
   } else {
     next();
   }
+<%_ } else { _%>
+  next();
+<%_ } _%>
 });
