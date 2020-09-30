4e05417e29b420758579d1d69d398b8c25a9c6c2
cklwblove <lwbhtml@163.com>
2020-09-30 17:25:10 +0800

    refactor(LOCAL_CONFIG): PC模式下，删除IS_OPEN_VCONSOLE属性

--- generator/ts-template/public/config.local.js
+++ generator/ts-template/public/config.local.js
@@ -1,5 +1,6 @@
 window.LOCAL_CONFIG = {
-  API_HOME: 'https://api.github.com/',
+  API_HOME: 'https://api.github.com/'<%_ if (options.application !== 'pc') { _%>,
   // vconsole 开关
   IS_OPEN_VCONSOLE: true
+  <%_ } _%>
 }
