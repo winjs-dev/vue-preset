bae1a78e6fa9ec78bc5964eb43a87163d5eba6ca
cklwblove <lwbhtml@163.com>
2021-03-03 13:58:08 +0800

    refactor(code): less-loader 升级7.3.0 版本，忽略 @vue/cli-service 报的warning

--- generator/utils.js
+++ generator/utils.js
@@ -1,28 +1,15 @@
 const fs = require('fs');
+const rimraf = require('rimraf');
 
 module.exports = (api) => {
   return {
     deleteFile(path) {
       const file = api.resolve(path);
-      if (fs.existsSync(file)) {
-        fs.unlinkSync(file);
-      }
+      rimraf.sync(file);
     },
     deleteDir(path) {
       const dir = api.resolve(path);
-      if (fs.existsSync(dir)) {
-        fs.readdirSync(dir).forEach((o) => {
-          const file = dir + '\\' + o;
-          if (fs.statSync(file).isDirectory()) {
-            fs.readdirSync(dir).forEach((p) => {
-              fs.unlinkSync(dir + '\\' + o + '\\' + p);
-            });
-          } else {
-            fs.unlinkSync(file);
-          }
-        });
-        fs.rmdirSync(dir);
-      }
+      rimraf.sync(dir);
     }
   };
 };
--- prompts.js
+++ prompts.js
@@ -6,16 +6,16 @@
  */
 const pcUI = [
   {
-    name: "Element UI",
-    value: "element-ui"
+    name: 'Element UI',
+    value: 'element-ui'
   },
   {
-    name: "ant-design-vue",
-    value: "ant"
+    name: 'ant-design-vue',
+    value: 'ant'
   },
   {
-    name: "none",
-    value: "none"
+    name: 'none',
+    value: 'none'
   }
 ];
 
@@ -23,128 +23,129 @@ const pcUI = [
 const pcUI2 = [
   ...pcUI,
   {
-    name: "h_ui",
-    value: "hui"
+    name: 'h_ui',
+    value: 'hui'
   },
   {
-    name: "iView",
-    value: "iview"
+    name: 'iView',
+    value: 'iview'
   }
 ];
 
 module.exports = [
   {
-    name: "preset",
-    type: "list",
-    message: "Please pick a preset",
+    name: 'preset',
+    type: 'list',
+    message: 'Please pick a preset',
     choices: [
       {
-        name: "Vue 2",
-        value: "v2"
+        name: 'Vue 2',
+        value: 'v2'
       },
       {
-        name: "Vue 3 Preview",
-        value: "v3"
+        name: 'Vue 3',
+        value: 'v3'
       }
     ],
-    default: "v2"
+    default: 'v2'
   },
   {
-    name: "language",
-    type: "list",
-    message: "Choose whether your develop language is a JS or TS(default:JS)",
+    name: 'language',
+    type: 'list',
+    message: 'Choose whether your develop language is a JS or TS(default:JS)',
     choices: [
       {
-        name: "JS",
-        value: "js"
+        name: 'JS',
+        value: 'js'
       },
       {
-        name: "TS",
-        value: "ts"
+        name: 'TS',
+        value: 'ts'
       }
     ],
-    default: "js"
+    default: 'js'
   },
   {
-    name: "application",
-    type: "list",
-    message: "Choose whether your app is a PC or a mobile(default:mobile)",
+    name: 'application',
+    type: 'list',
+    message: 'Choose whether your app is a PC or a mobile(default:mobile)',
     choices: [
       {
-        name: "PC",
-        value: "pc"
+        name: 'PC',
+        value: 'pc'
       },
       {
-        name: "mobile",
-        value: "mobile"
+        name: 'mobile',
+        value: 'mobile'
       },
       {
-        name: "H5离线包",
-        value: "offline"
+        name: 'H5离线包',
+        value: 'offline'
       }
     ],
-    default: "mobile"
+    default: 'mobile'
   },
   {
-    name: "offline-id",
-    type: "input",
-    message: "Fill in the offline package ID",
+    name: 'offline-id',
+    type: 'input',
+    message: 'Fill in the offline package ID',
     validate: function (val) {
       return /^[0-9a-z]{4,8}$/.test(val)
         ? true
-        : "请输入4-8位的小写英文字母或数字，注意唯一性";
+        : '请输入4-8位的小写英文字母或数字，注意唯一性';
     },
-    when: (answers) => answers.application === "offline",
-    default: "离线包 ID"
+    when: (answers) => answers.application === 'offline',
+    default: '离线包 ID'
   },
   {
-    name: "offline-name",
-    type: "input",
-    message: "Fill in the offline package name",
+    name: 'offline-name',
+    type: 'input',
+    message: 'Fill in the offline package name',
     validate: function (val) {
       return /^[\u4e00-\u9fa5a-zA-Z0-9]{1,10}$/.test(val)
         ? true
-        : "请输入1-10位的中英文字符或数字";
+        : '请输入1-10位的中英文字符或数字';
     },
-    when: (answers) => answers.application === "offline",
-    default: "离线包中文描述"
+    when: (answers) => answers.application === 'offline',
+    default: '离线包中文描述'
   },
   {
-    name: "ui-framework",
-    type: "list",
-    message: "Choice UI Framework(default:none)",
-    choices: (answers) => (answers.preset === "v2" ? pcUI2 : pcUI),
-    when: (answers) => answers.application === "pc",
-    default: "none"
+    name: 'ui-framework',
+    type: 'list',
+    message: 'Choice UI Framework(default:none)',
+    choices: (answers) => (answers.preset === 'v2' ? pcUI2 : pcUI),
+    when: (answers) => answers.application === 'pc',
+    default: 'none'
   },
   {
-    name: "mobile-ui-framework",
-    type: "list",
-    message: "Choice Mobile UI Framework(default:none)",
+    name: 'mobile-ui-framework',
+    type: 'list',
+    message: 'Choice Mobile UI Framework(default:none)',
     choices: [
       {
-        name: "Vant",
-        value: "vant"
+        name: 'Vant',
+        value: 'vant'
       },
       {
-        name: "none",
-        value: "none"
+        name: 'none',
+        value: 'none'
       }
     ],
     when: (answers) =>
-      answers.application === "mobile" || answers.application === "offline",
-    default: "none"
+      answers.application === 'mobile' || answers.application === 'offline',
+    default: 'none'
   },
   {
-    name: "mirror-source",
-    type: "confirm",
-    message: "Whether it is an internal project of the company?",
+    name: 'mirror-source',
+    type: 'confirm',
+    message: 'Whether it is an internal project of the company?',
     initial: true
   },
   {
-    name: "see-package",
-    type: "confirm",
-    message: "Whether to support the generation of see platform publications?",
+    name: 'see-package',
+    type: 'confirm',
+    message: 'Whether to support the generation of see platform publications?',
+    when: (answers) => answers['mirror-source'] === true,
     initial: true
   }
 ];
