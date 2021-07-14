e8cc29f9358dc6c24dc4af38d7f9843579e8011d
cklwblove <lwbhtml@163.com>
2021-07-14 16:23:17 +0800

    refactor(request.js): 优化写法

--- generator/template-vue2/src/services/index.js
+++ generator/template-vue2/src/services/index.js
@@ -12,6 +12,11 @@ Object.keys(urls).forEach((key) => {
 
 // 将services挂载到vue的原型上
 // views引用的方法：this.$services.接口名（小驼峰）
-Object.defineProperty(Vue.prototype, '$services', { value: FUNS });
+// 防止被篡改
+Object.defineProperty(Vue.prototype, '$services', {
+  get () {
+    return FUNS;
+  }
+});
 
 export default FUNS;
