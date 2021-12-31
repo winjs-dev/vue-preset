0a6189952f5b21f6642709e2a9adcef6b7ff9e2c
cklwblove <lwbhtml@163.com>
2021-12-31 17:52:10 +0800

    fix(checkishwscontainer): 判断方法bug

--- generator/subsystem/src/utils/index.js
+++ generator/subsystem/src/utils/index.js
@@ -1,4 +1,4 @@
 // 检测是否是财富中台外框架
 export function checkIsHwsContainer() {
-  return typeof window.fetch && window.fetch.post === 'function';
+  return window.fetch && typeof window.fetch.post === 'function';
 }
