93967f960e9c1debe7be03baf8ab3f077a7f6f0e
cklwblove <lwbhtml@163.com>
2022-01-28 17:05:43 +0800

    refactor(subsystem): 子系统 app.vue 更新

--- generator/subsystem/src/App.vue
+++ generator/subsystem/src/App.vue
@@ -1,22 +1,39 @@
 <template>
   <div id="app">
-    <QuickNavigation />
-    <div class="pages">
-      <keep-alive v-if="$route.meta.keepAlive">
-        <router-view />
-      </keep-alive>
-      <router-view v-if="!$route.meta.keepAlive" />
-    </div>
+    <frame-layout v-if="isFrame">
+      <div class="pages">
+        <keep-alive v-if="$route.meta.keepAlive">
+          <router-view />
+        </keep-alive>
+        <router-view v-if="!$route.meta.keepAlive" />
+      </div>
+    </frame-layout>
+    <template v-else>
+      <QuickNavigation />
+      <div class="pages">
+        <keep-alive v-if="$route.meta.keepAlive">
+          <router-view />
+        </keep-alive>
+        <router-view v-if="!$route.meta.keepAlive" />
+      </div>
+    </template>
   </div>
 </template>
 
 <script>
-  import { QuickNavigation } from '@/components';
+import { QuickNavigation, FrameLayout } from '@/components';
 
-  export default {
-    name: 'App',
-    components: {
-      QuickNavigation
-    }
-  };
+export default {
+  name: 'App',
+  components: {
+    QuickNavigation,
+    FrameLayout
+  },
+  data() {
+    return {
+      // 是否使用财富中台外框架
+      isFrame: true
+    };
+  }
+};
 </script>
